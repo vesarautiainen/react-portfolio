3471ffc416bb22f059534d0bb6663edcc16df56c
Vesa Rautiainen <vrautiainen@gmail.com>
2017-09-13 14:12:48 +0300

    adding missing files

---
+++ src/MainArea.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+//import projectLogo from './project.svg';
+import './MainArea.css';
+
+class MainArea extends Component {
+  render() {
+    return (
+      <div className="wrapper">
+        <div className="one">One</div>
+        <div className="two">Two</div>
+        <div className="three">Three</div>
+        <div className="four">Four</div>
+        <div className="five">Five</div>
+        <div className="six">Six</div>
+      </div>
+    );
+  }
+}
+
+export default MainArea;
